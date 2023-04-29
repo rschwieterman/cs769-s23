26416200d10eaf9373281c9c8cec3ac6963720f1
spencrr <23708360+spencrr@users.noreply.github.com>
2023-04-29 16:53:35 -0500

    fixup tab

--- src/transformers/test.py
+++ src/transformers/test.py
@@ -121,25 +121,25 @@ optimizer = optim.AdamW(model.parameters(), lr=1e-5)
 print("begin training")
 
 for epoch in range(10):
-  epoch_loss = 0.0
-  v_epoch_loss = 0.0
-  for batch_data in tqdm(train_dataloader):
-    optimizer.zero_grad()
-    outter2 = model.forward(batch_data[0].to(device), decoder_input_ids=batch_data[1].to(device), decoder_attention_mask=batch_data[2].to(device))
-    loss = loss_func(outter2.cls_logit_out, batch_data[3].to(device), batch_data[2].to(device))
-    loss.backward()
-    epoch_loss += loss.detach().cpu().item()
-    optimizer.step()
-  for val_data in tqdm(val_dataloader):
-    optimizer.zero_grad()
-    #print("i need to know about data")
-    voutputs = model.forward(val_data[0].to(device).detach(), decoder_input_ids=val_data[1].to(device).detach(), decoder_attention_mask=val_data[2].to(device).detach())
-    #voutputs = voutputs.detach()
-    vloss = loss_func(voutputs.cls_logit_out, val_data[3].to(device), val_data[2].to(device))
-    v_epoch_loss += vloss
-  print(epoch_loss)
-  print(epoch_loss/len(train_dataloader))
-  print(v_epoch_loss/len(val_dataloader))
+    epoch_loss = 0.0
+    v_epoch_loss = 0.0
+    for batch_data in tqdm(train_dataloader):
+        optimizer.zero_grad()
+        outter2 = model.forward(batch_data[0].to(device), decoder_input_ids=batch_data[1].to(device), decoder_attention_mask=batch_data[2].to(device))
+        loss = loss_func(outter2.cls_logit_out, batch_data[3].to(device), batch_data[2].to(device))
+        loss.backward()
+        epoch_loss += loss.detach().cpu().item()
+        optimizer.step()
+    for val_data in tqdm(val_dataloader):
+        optimizer.zero_grad()
+        #print("i need to know about data")
+        voutputs = model.forward(val_data[0].to(device).detach(), decoder_input_ids=val_data[1].to(device).detach(), decoder_attention_mask=val_data[2].to(device).detach())
+        #voutputs = voutputs.detach()
+        vloss = loss_func(voutputs.cls_logit_out, val_data[3].to(device), val_data[2].to(device))
+        v_epoch_loss += vloss
+    print(epoch_loss)
+    print(epoch_loss/len(train_dataloader))
+    print(v_epoch_loss/len(val_dataloader))
 
 
 
